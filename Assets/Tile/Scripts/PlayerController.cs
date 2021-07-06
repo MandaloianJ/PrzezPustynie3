17527807abb5f83d26145e32451facbee1fb4cc3
MandaloianJ <jan.chelstowski.tv@gmail.com>
2021-07-06 12:20:36 +0200

    .

--- Assets/Tile/Scripts/PlayerController.cs
+++ Assets/Tile/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerController : MonoBehaviour
 {
 
@@ -34,6 +36,7 @@ public class PlayerController : MonoBehaviour
         onTheGround = Physics2D.OverlapCircle(groundTester.position, radius, layearsToTest);
 
         float horizontalMove = Input.GetAxis("Horizontal");
+        Debug.Log("horizontalMove:" + horizontalMove);
         rgdBody.velocity = new Vector2 (horizontalMove * heroSpeed, rgdBody.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space) && onTheGround) {
@@ -63,6 +66,27 @@ public class PlayerController : MonoBehaviour
         Vector3 heroScale = gameObject.transform.localScale;
         heroScale.x *= -1;
         gameObject.transform.localScale = heroScale;
+    }
+
+    public void idzwlewo()
+    {
+        //OpenLevel(currentLevel);
+        Debug.Log("idz w lewo");
+    }
+
+    public void idzwprawo()
+    {
+        //OpenLevel(currentLevel);
+        Debug.Log("idz w prawo");
+
+        Vector3 heroScale = gameObject.transform.localScale;
+        heroScale.x *= -1;
+        gameObject.transform.localScale = heroScale;
+    }
+    public void skacz()
+    {
+        //OpenLevel(currentLevel);
+        Debug.Log("skacz");
     }
 }
 
