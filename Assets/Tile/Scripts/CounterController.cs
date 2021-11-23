9b831731ee5893c6be4ed0980957fa93a4b74bb2
MandaloianJ <jan.chelstowski.tv@gmail.com>
2021-11-23 20:14:43 +0100

    c

--- Assets/Tile/Scripts/CounterController.cs
+++ Assets/Tile/Scripts/CounterController.cs
@@ -6,13 +6,14 @@ using UnityEngine.UI;
 public class CounterController : MonoBehaviour
 {
 
-     int numberOfBoxes;
+     int numberOfBoxes =0;
     public Text counterView;
+    public Text counterView2;
 
     // Start is called before the first frame update
     void Start()
     {
-    
+        counterView.text = numberOfBoxes.ToString();
     }
     public void IncrementCounter()
     {
