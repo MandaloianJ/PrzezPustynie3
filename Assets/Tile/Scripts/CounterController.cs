a38902754f4a5d3aea2c450a3be42307efdcbb93
MandaloianJ <jan.chelstowski.tv@gmail.com>
2021-11-24 09:06:16 +0100

    3

--- Assets/Tile/Scripts/CounterController.cs
+++ Assets/Tile/Scripts/CounterController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class CounterController : MonoBehaviour
 {
@@ -9,17 +10,27 @@ public class CounterController : MonoBehaviour
      double numberOfBoxes =0;
     public Text counterView;
     public Text counterView2;
+    DateTime lastevent;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         ResetCounter();
+        lastevent = DateTime.Now;
     }
     public void IncrementCounter()
     {
         Debug.LogError("numberOfBoxes:" + numberOfBoxes);
-        numberOfBoxes = numberOfBoxes+ 1;
-        counterView.text = numberOfBoxes.ToString();
+
+        if ((DateTime.Now - lastevent).TotalMilliseconds > 250)
+        {
+            numberOfBoxes = numberOfBoxes + 1;
+            counterView.text = numberOfBoxes.ToString();
+            lastevent = DateTime.Now;
+        }
+        
+        
     }
     public void ResetCounter()
     {
